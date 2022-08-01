8ddffb690baca4a120cd235223966be6152ccc01
Ruslan Gaziev <grinrus@yandex.ru>
2022-08-01 22:54:06 +0300

    5 Route for login and registration done

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,14 @@ const {
   createUser,
   updateUser,
   updateAvatar,
+  login,
 } = require('../controllers/users');
 
 router.get('/users', getAllUsers);
 router.get('/users/:userId', getUserById);
-router.post('/users', createUser);
 router.patch('/users/me', updateUser);
 router.patch('/users/me/avatar', updateAvatar);
+router.post('/signin', login);
+router.post('/signup', createUser);
 
 module.exports = router;
