2c96188e8ee0085565289e8996191e705a1a9465
Ruslan Gaziev <grinrus@yandex.ru>
2022-08-03 18:05:36 +0300

    All done

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const routerCards = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
 
 const {
@@ -6,9 +7,18 @@ const {
 } = require('../controllers/cards');
 
 routerCards.get('/cards', auth, getAllCards);
-routerCards.post('/cards', auth, createCard);
+
+routerCards.post('/cards', auth, celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    link: Joi.string().pattern(/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/),
+  }),
+}), createCard);
+
 routerCards.delete('/cards/:cardId', auth, deleteCardById);
+
 routerCards.put('/cards/:cardId/likes', auth, likeCard);
+
 routerCards.delete('/cards/:cardId/likes', auth, dislikeCard);
 
 module.exports = routerCards;
--- routes/users.js
+++ routes/users.js
@@ -13,11 +13,36 @@ const {
 } = require('../controllers/users');
 
 routerUsers.get('/users', auth, getAllUsers);
+
 routerUsers.get('/users/:userId', auth, getUserById);
-routerUsers.patch('/users/me', auth, updateUser);
-routerUsers.patch('/users/me/avatar', auth, updateAvatar);
-routerUsers.post('/signin', login);
-routerUsers.post('/signup', createUser);
+
+routerUsers.patch('/users/me', auth, celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUser);
+
+routerUsers.patch('/users/me/avatar', auth, celebrate({
+  body: Joi.object().keys({
+    link: Joi.string().pattern(/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/),
+  }),
+}), updateAvatar);
+
+routerUsers.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/),
+  }),
+}), login);
+
+routerUsers.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/),
+  }),
+}), createUser);
+
 routerUsers.get('/users/me', auth, getUserInfo);
 
 module.exports = routerUsers;
