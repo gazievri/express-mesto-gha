6f470efea3e7efcd89412bf0d3c9a1f053935db2
Ruslan Gaziev <grinrus@yandex.ru>
2022-08-05 16:43:22 +0300

    My god!!!

--- routes/users.js
+++ routes/users.js
@@ -52,7 +52,7 @@ routerUsers.post('/signup', celebrate({
     password: Joi.string().required().pattern(/^[a-zA-Z0-9]{3,30}$/),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().required().pattern(/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-/]))?/),
+    avatar: Joi.string().pattern(/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-/]))?/),
   }),
 }), createUser);
 
